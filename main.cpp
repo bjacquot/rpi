cc9ee2ca0986239bf2f054856b03b27ed18764ac
Bastien Jacquot <bastien.jacquot@univ-reims.fr>
2016-11-17 14:55:51 +0100

    ex PWM

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <QCoreApplication>
+#include "io/pwm.h"
+#include "io/pwmclk.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    PwmClk *pwmConf= new PwmClk();
+    pwmConf->setPwmClkDiv(32);
+    Pwm *moteurG=new Pwm(0);
+    Pwm *moteurD=new Pwm(1);
 
-    return a.exec();
+    moteurD->setRange(2000);
+    moteurG->setRange(2000);
+
+    moteurD->setRapportCyclique(1500);
+    moteurG->setRapportCyclique(500);
+
+    return 0;
+
 }
